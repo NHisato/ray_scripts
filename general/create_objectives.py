054b2e4d1cb46c4e86e4cd5837423cd4f68a67f9
Adam Bayliss <rabayliss@wisc.edu>
2019-01-08 16:16:03 -0600

    Update create_objectives.py
    debug

--- general/create_objectives.py
+++ general/create_objectives.py
@@ -184,7 +184,7 @@ def add_objective(obj, case, plan, beamset,
             opts.OptimizedBeamSets[beamset.DicomPlanLabel]
             indices.append(OptIndex)
         #Not sure of error type
-        except AttributeError:
+        except TypeError:
             pass
     # Ensure we have a unique match or exit
     if len(indices) == 1:
